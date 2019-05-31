8dc14ce090b7811c5ae6c1d9aebb5f0b6ea37fef
Diana Hervás Castillo <dianahervascastillo@users.noreply.github.com>
2019-05-31 16:55:25 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var activePots = require('./draftLabel.json');
+var labels = require('./draftLabel.json');
 
 var SERVER_PORT = 3004;
 
 app.get('/draft-label', function(request, response) {
   response.setHeader('Content-Type', 'application/json');
-  response.send(JSON.stringify(activePots));
+  response.send(JSON.stringify(labels));
 });
 
 app.listen(SERVER_PORT, () => console.log('Order service listening on http://localhost:' + SERVER_PORT));
